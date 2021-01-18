468db8ddbf9225ae984a03add908b40613fbc3e3
GodfredAsa <77088001+GodfredAsa@users.noreply.github.com>
2021-01-18 05:14:02 +0000

    initial commit

---
+++ DayOfTheWeek.java
@@ -0,0 +1,39 @@
+package ProgrammingLab;
+
+public class DayOfTheWeek {
+    public static void printDayOfTheWeek(int day){
+        
+        switch(day){
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Not a day of the Week.");
+                break;
+        }
+    }
+
+
+    public static void main(String[] args){
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(6);
+    }
+}
